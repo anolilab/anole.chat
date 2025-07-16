4832b54589d5e9d0aa9337df14185b629bdf3994
prisis <d.bannert@anolilab.de>
2025-07-16 15:24:51 +0200

    chore: hide files for now

--- app/src/features/layout/components/app-sidebar.tsx
+++ app/src/features/layout/components/app-sidebar.tsx
@@ -28,11 +28,13 @@ const AppSidebar = ({
             matcher: (pathname) => pathname.startsWith("/chat"),
             to: "/chat",
         },
+        /**
         {
             icon: File,
             label: t`Files`,
             to: "/files",
         },
+        */
         {
             icon: Cog,
             label: t`Open Account Settings`,
